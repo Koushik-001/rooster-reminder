3f8601c02dcf5f05aa453a85f089e8ffd45b1d0b
Koushik-001 <koushik.chv@gmail.com>
2026-09-01 22:32:08 +0530

    updated homepage and packages

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { ScreenContent } from 'components/ScreenContent';
+import { HomePage } from 'components/HomePage';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
@@ -7,7 +7,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+      <HomePage title="Home" path="App.tsx"></HomePage>
       <StatusBar style="auto" />
     </SafeAreaProvider>
   );
--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,24 +1,27 @@
 
-import {User} from 'lucide-react-native';
+import { User } from 'lucide-react-native';
 import React, { useEffect, useState } from 'react';
 import { Pressable, Text, View } from 'react-native';
 
 import { Calendar } from 'react-native-calendars';
 import Animated, { FadeIn, FadeInDown, FadeInUp, FadeOutUp } from 'react-native-reanimated';
+import { usePathname, useRouter } from "expo-router";
 
 
-interface ScreenContentProps {
+interface HomePageProps {
   title: string;
   path: string;
   children?: React.ReactNode;
 }
 
-export const ScreenContent: React.FC<ScreenContentProps> = ({ title, path, children }) => {
+export const HomePage: React.FC<HomePageProps> = ({ title, path, children }) => {
   const today = new Date();
   const date = today.getDate();
   const month = today.toLocaleString('default', { month: 'long' });
   const [calendarClicked, setcalendarClicked] = useState(false);
-
+  // const pathname = usePathname();
+  // const router = useRouter();
+  // console.log(pathname, 'name');
   return (
     <View className="bg-white flex-1 pt-[5vh]">
       <View className='flex-row px-5 justify-between'>
@@ -44,8 +47,11 @@ export const ScreenContent: React.FC<ScreenContentProps> = ({ title, path, child
           <View className='border border-black-100/30 rounded-lg p-2 m-5 top-[5vh]' ><Calendar enableSwipeMonths /></View>
         </Animated.View>
         : ""}
-      <View className='absolute top-[90vh] left-[45vw] items-center'>
-        <Text>nav bar</Text>
+      <View className='absolute top-[90vh] items-center w-full items-center'>
+        <View className='border border-black-500 rounded-3xl p-3 w-[50%] justify-between flex-row'>
+          <Pressable className='bg-red-500 active:bg-black'><Text >nav bar</Text></Pressable>
+          <Text>nav bar</Text>
+        </View>
       </View>
     </View>
   );
